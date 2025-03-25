7d2f190c064e979b286f9ce7bc0a7123fcf8c5e4
kimgeonsu <kundoo0412@gmail.com>
2025-03-25 16:06:37 +0900

    cpu 반복

--- lab3/src/add_loop_cpu.cu
+++ lab3/src/add_loop_cpu.cu
@@ -1,7 +1,7 @@
 #include "../common/book.h"
-#define N 10
+#define MAX_N 10000
 
-void add( int *a, int *b, int *c) {
+void add(int *a, int *b, int *c, int N) {
     int tid = 0;
     while(tid < N) {
         c[tid] = a[tid] + b[tid];
@@ -9,18 +9,25 @@ void add( int *a, int *b, int *c) {
     }
 }
 
-int main( void ) {
-    int a[N], b[N], c[N];
-    for (int i = 0; i < N; i++) {
-        a[i] = -i;
-        b[i] = i*i;
-    }
-    clock_t start = clock();
-    add(a, b, c);
-    clock_t end = clock();
-    printf("소요 시간 :%lf\n", (double)(end - start) / CLOCKS_PER_SEC);
-    for (int i=0; i<N; i++) {
-        printf("%d + %d = %d\n", a[i], b[i], c[i]);
+int main(void) {
+    for(int N = 1000; N <= MAX_N; N += 1000) {
+        int *a = (int*)malloc(N * sizeof(int));
+        int *b = (int*)malloc(N * sizeof(int));
+        int *c = (int*)malloc(N * sizeof(int));
+
+        for (int i = 0; i < N; i++) {
+            a[i] = -i;
+            b[i] = i*i;
+        }
+
+        clock_t start = clock();
+        add(a, b, c, N);
+        clock_t end = clock();
+        printf("N=%d일 때 소요 시간: %lf\n", N, (double)(end - start) / CLOCKS_PER_SEC);
+
+        free(a);
+        free(b);
+        free(c);
     }
 
     return 0;
