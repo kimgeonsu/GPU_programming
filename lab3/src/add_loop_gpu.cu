382cccf236ce341cea5187adb24956d0a33cf22b
kimgeonsu <kundoo0412@gmail.com>
2025-03-25 16:01:15 +0900

    반복

--- lab3/src/add_loop_gpu.cu
+++ lab3/src/add_loop_gpu.cu
@@ -1,41 +1,50 @@
 #include <stdio.h>
 #include "../common/book.h"
 
-#define N 10000
+#define MAX_N 10000
 
-__global__ void add(int *a, int *b, int *c) {
+__global__ void add(int *a, int *b, int *c, int N) {
     int tid = blockIdx.x;
     if (tid < N)
         c[tid] = a[tid] + b[tid];
 }
 
-int main( void ) {
-    int a[N], b[N], c[N];
+int main(void) {
+    int *a, *b, *c;
     int *dev_a, *dev_b, *dev_c;
-    HANDLE_ERROR( cudaMalloc((void**)&dev_a, N * sizeof(int)));
-    HANDLE_ERROR( cudaMalloc((void**)&dev_b, N * sizeof(int)));
-    HANDLE_ERROR( cudaMalloc((void**)&dev_c, N * sizeof(int)));
 
-    for (int i=0; i < N; i++) {
-        a[i] = -i;
-        b[i] = i*i;
+    for(int N = 1000; N <= MAX_N; N += 1000) {
+        a = (int*)malloc(N * sizeof(int));
+        b = (int*)malloc(N * sizeof(int));
+        c = (int*)malloc(N * sizeof(int));
+
+        HANDLE_ERROR(cudaMalloc((void**)&dev_a, N * sizeof(int)));
+        HANDLE_ERROR(cudaMalloc((void**)&dev_b, N * sizeof(int)));
+        HANDLE_ERROR(cudaMalloc((void**)&dev_c, N * sizeof(int)));
+
+        for (int i = 0; i < N; i++) {
+            a[i] = -i;
+            b[i] = i*i;
+        }
+
+        HANDLE_ERROR(cudaMemcpy(dev_a, a, N*sizeof(int), cudaMemcpyHostToDevice));
+        HANDLE_ERROR(cudaMemcpy(dev_b, b, N*sizeof(int), cudaMemcpyHostToDevice));
+        
+        clock_t start = clock();
+        add<<<N, 1>>>(dev_a, dev_b, dev_c, N);
+        clock_t end = clock();
+        printf("N=%d일 때 소요 시간: %lf\n", N, (double)(end - start) / CLOCKS_PER_SEC);
+
+        HANDLE_ERROR(cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost));
+        
+        HANDLE_ERROR(cudaFree(dev_a));
+        HANDLE_ERROR(cudaFree(dev_b));
+        HANDLE_ERROR(cudaFree(dev_c));
+        
+        free(a);
+        free(b);
+        free(c);
     }
-
-    HANDLE_ERROR(cudaMemcpy(dev_a, a, N*sizeof(int), cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemcpy(dev_b, b, N*sizeof(int), cudaMemcpyHostToDevice));
-    clock_t start = clock();
-    add<<<N, 1>>>(dev_a, dev_b, dev_c);
-    clock_t end = clock();
-    printf("소요 시간:%lf\n", (double)(end - start) / CLOCKS_PER_SEC);
-
-    HANDLE_ERROR(cudaMemcpy(dev_c, c, N*sizeof(int), cudaMemcpyHostToDevice));
-    // for (int i =0; i<N;i++) {
-    //     printf("%d + %d = %d\n", a[i], b[i], c[i]);
-    // }
-
-    HANDLE_ERROR(cudaFree(dev_a));
-    HANDLE_ERROR(cudaFree(dev_b));
-    HANDLE_ERROR(cudaFree(dev_c));
 
     return 0;
 }
